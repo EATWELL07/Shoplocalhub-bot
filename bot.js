3c8cce46be6e59aa3f779349af6fb854a64c632a
Yogesh <yogibhatia22@gmail.com>
2026-05-11 14:50:27 +0530

    clean puppeteer config

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
